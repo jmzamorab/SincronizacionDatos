030ad49c893dea27622316cceb31763990b0fefb
jmzamorab <josem.zamorab@gmail.com>
2017-07-02 09:17:46 +0200

    Sincronización de datos con un Contador

--- mobile/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
+++ mobile/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
@@ -6,18 +6,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
 public class MainActivity extends AppCompatActivity {
     private static final String WEAR_MANDAR_TEXTO = "/mandar_texto";
     private GoogleApiClient apiClient;
-    private static final String WEAR_ARRANCAR_ACTIVIDAD="/arrancar_actividad";
+    private static final String WEAR_ARRANCAR_ACTIVIDAD = "/arrancar_actividad";
+    private static final String KEY_CONTADOR = "com.example.key.contador";
+    private static final String ITEM_CONTADOR = "/contador";
+    private int contador = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mandarMensaje(WEAR_ARRANCAR_ACTIVIDAD, "");
+            }
+        });
+
+        final TextView textoContador = (TextView) findViewById(R.id.textoContador);
+        textoContador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador++;
+                textoContador.setText(Integer.toString(contador));
+                PutDataMapRequest putDataMapReq = PutDataMapRequest.create(ITEM_CONTADOR);
+                putDataMapReq.getDataMap().putInt(KEY_CONTADOR, contador);
+                PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+                PendingResult<DataApi.DataItemResult> resultado = Wearable.DataApi.putDataItem(apiClient, putDataReq);
             }
         });
     }
--- wear/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
+++ wear/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
@@ -5,15 +5,25 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Wearable;
 
-public class MainActivity extends Activity implements MessageApi.MessageListener, GoogleApiClient.ConnectionCallbacks {
+public class MainActivity extends Activity implements MessageApi.MessageListener, DataApi.DataListener,
+        GoogleApiClient.ConnectionCallbacks {
 
     private static final String WEAR_MANDAR_TEXTO = "/mandar_texto";
     private GoogleApiClient apiClient;
     private TextView textView;
+    private static final String KEY_CONTADOR = "com.example.key.contador";
+    private static final String ITEM_CONTADOR = "/contador";
+    private int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,7 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
         if (apiClient != null && apiClient.isConnected()) {
             apiClient.disconnect();
         }
+        Wearable.DataApi.removeListener(apiClient, this);
         super.onStop();
     }
 
@@ -42,11 +53,34 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
     @Override
     public void onConnected(Bundle bundle) {
         Wearable.MessageApi.addListener(apiClient, this);
+        Wearable.DataApi.addListener(apiClient, this);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
     }
+
+    @Override
+    public void onDataChanged(DataEventBuffer eventos) {
+        for (DataEvent evento : eventos) {
+            if (evento.getType() == DataEvent.TYPE_CHANGED) {
+                DataItem item = evento.getDataItem();
+                if (item.getUri().getPath().equals(ITEM_CONTADOR)) {
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    contador = dataMap.getInt(KEY_CONTADOR);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((TextView) findViewById(R.id.textoContador)).setText(Integer.toString(contador));
+                        }
+                    });
+                }
+            } else if (evento.getType() == DataEvent.TYPE_DELETED) {
+                // Algún ítem ha sido borrado
+            }
+        }
+    }
+
 
     @Override
     public void onMessageReceived(final MessageEvent mensaje) {
