e2f282ea14a47fe5d2c0dab3f35364d5f269b4e5
jmzamorab <josem.zamorab@gmail.com>
2017-07-03 23:53:26 +0200

    Mandar Foto

--- mobile/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
+++ mobile/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
@@ -1,5 +1,7 @@
 package es.upv.master.sincronizaciondatos;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,6 +13,7 @@ import android.widget.TextView;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.Node;
@@ -19,6 +22,9 @@ import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     private static final String WEAR_MANDAR_TEXTO = "/mandar_texto";
     private GoogleApiClient apiClient;
@@ -26,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String KEY_CONTADOR = "com.example.key.contador";
     private static final String ITEM_CONTADOR = "/contador";
     private int contador = 0;
-
+    private static final String ITEM_FOTO = "/item_foto";
+    private static final String ASSET_FOTO = "/asset_foto";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +69,30 @@ public class MainActivity extends AppCompatActivity {
                 PendingResult<DataApi.DataItemResult> resultado = Wearable.DataApi.putDataItem(apiClient, putDataReq);
             }
         });
+
+        Button botonFoto = (Button) findViewById(R.id.botonFoto);
+        botonFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mandarFoto();
+            }
+        });
+    }
+
+    private void mandarFoto() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.foto);
+        Asset asset = createAssetFromBitmap(bitmap);
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create(ITEM_FOTO);
+        putDataMapReq.getDataMap().putAsset(ASSET_FOTO, asset);
+        putDataMapReq.getDataMap().putLong("marca_de_tiempo", new Date().getTime());
+        PutDataRequest request = putDataMapReq.asPutDataRequest();
+        PendingResult<DataApi.DataItemResult> resultdado = Wearable.DataApi.putDataItem(apiClient, request);
+    }
+
+    private static Asset createAssetFromBitmap(Bitmap bitmap) {
+        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream);
+        return Asset.createFromBytes(byteStream.toByteArray());
     }
 
     @Override
--- wear/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
+++ wear/src/main/java/es/upv/master/sincronizaciondatos/MainActivity.java
@@ -1,12 +1,20 @@
 package es.upv.master.sincronizaciondatos;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
@@ -18,6 +26,9 @@ import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Wearable;
 
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends Activity implements MessageApi.MessageListener, DataApi.DataListener,
         GoogleApiClient.ConnectionCallbacks {
 
@@ -27,6 +38,8 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
     private static final String KEY_CONTADOR = "com.example.key.contador";
     private static final String ITEM_CONTADOR = "/contador";
     private int contador = 0;
+    private static final String ITEM_FOTO = "/item_foto";
+    private static final String ASSET_FOTO = "/asset_foto";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,65 +69,94 @@ public class MainActivity extends Activity implements MessageApi.MessageListener
         });
     }
 
-        @Override
-        protected void onStart () {
-            super.onStart();
-            apiClient.connect();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        apiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        Wearable.MessageApi.removeListener(apiClient, this);
+        if (apiClient != null && apiClient.isConnected()) {
+            apiClient.disconnect();
         }
-
-        @Override
-        protected void onStop () {
-            Wearable.MessageApi.removeListener(apiClient, this);
-            if (apiClient != null && apiClient.isConnected()) {
-                apiClient.disconnect();
-            }
-            Wearable.DataApi.removeListener(apiClient, this);
-            super.onStop();
-        }
+        Wearable.DataApi.removeListener(apiClient, this);
+        super.onStop();
+    }
 
 
-        @Override
-        public void onConnected (Bundle bundle){
-            Wearable.MessageApi.addListener(apiClient, this);
-            Wearable.DataApi.addListener(apiClient, this);
-        }
+    @Override
+    public void onConnected(Bundle bundle) {
+        Wearable.MessageApi.addListener(apiClient, this);
+        Wearable.DataApi.addListener(apiClient, this);
+    }
 
-        @Override
-        public void onConnectionSuspended ( int i){
-        }
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
 
-        @Override
-        public void onDataChanged (DataEventBuffer eventos){
-            for (DataEvent evento : eventos) {
-                if (evento.getType() == DataEvent.TYPE_CHANGED) {
-                    DataItem item = evento.getDataItem();
-                    if (item.getUri().getPath().equals(ITEM_CONTADOR)) {
-                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
-                        contador = dataMap.getInt(KEY_CONTADOR);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                ((TextView) findViewById(R.id.textoContador)).setText(Integer.toString(contador));
-                            }
-                        });
-                    }
-                } else if (evento.getType() == DataEvent.TYPE_DELETED) {
-                    // Algún ítem ha sido borrado
+    @Override
+    public void onDataChanged(DataEventBuffer eventos) {
+        for (DataEvent evento : eventos) {
+            if (evento.getType() == DataEvent.TYPE_CHANGED) {
+                DataItem item = evento.getDataItem();
+                if (item.getUri().getPath().equals(ITEM_CONTADOR)) {
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    contador = dataMap.getInt(KEY_CONTADOR);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((TextView) findViewById(R.id.textoContador)).setText(Integer.toString(contador));
+                        }
+                    });
+                } else if (item.getUri().getPath().equals(ITEM_FOTO)) {
+                    DataMapItem dataMapItem = DataMapItem.fromDataItem(item);
+                    Asset asset = dataMapItem.getDataMap().getAsset(ASSET_FOTO);
+                    LoadBitmapFromAsset tarea = new LoadBitmapFromAsset();
+                    tarea.execute(asset);
                 }
             }
         }
+    }
 
+    class LoadBitmapFromAsset extends AsyncTask<Asset, Void, Bitmap> {
+        private static final int TIMEOUT_MS = 2000;
 
         @Override
-        public void onMessageReceived ( final MessageEvent mensaje){
-            if (mensaje.getPath().equalsIgnoreCase(WEAR_MANDAR_TEXTO)) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        textView.setText(textView.getText() + "\n" + new String(mensaje.getData()));
-                    }
-                });
+        protected Bitmap doInBackground(Asset... assets) {
+            if (assets.length < 1 || assets[0] == null) {
+                throw new IllegalArgumentException("El asset no puede ser null");
             }
+
+            ConnectionResult resultado = apiClient.blockingConnect(TIMEOUT_MS, TimeUnit.MILLISECONDS);
+            if (!resultado.isSuccess()) {
+                return null;
+            } // convertimos el asset en Stream, bloqueando hasta tenerlo
+            InputStream assetInputStream = Wearable.DataApi.getFdForAsset(apiClient, assets[0]).await().getInputStream();
+            if (assetInputStream == null) {
+                Log.w("Sincronización", "Asset desconocido");
+                return null;
+            } // decodificamos el Stream en un Bitmap
+            return BitmapFactory.decodeStream(assetInputStream);
         }
 
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            ((BoxInsetLayout) findViewById(R.id.boxInsert)).setBackground(new BitmapDrawable(getResources(), bitmap));
+        }
     }
+
+    @Override
+    public void onMessageReceived(final MessageEvent mensaje) {
+        if (mensaje.getPath().equalsIgnoreCase(WEAR_MANDAR_TEXTO)) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(textView.getText() + "\n" + new String(mensaje.getData()));
+                }
+            });
+        }
+    }
+
+}
